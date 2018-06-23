ff0035ae9c331421085fda0c3ba4f3d7b1b3bcdb
Victor Pakhomov <victor@pakhomov.name>
2018-06-23 22:14:16 +0200

    using java @Configuration

---
+++ src/main/java/org/pakhomov/jokeapp/config/ChuckConfiguration.java
@@ -0,0 +1,19 @@
+package org.pakhomov.jokeapp.config;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.pakhomov.jokeapp.service.JokeService;
+import org.pakhomov.jokeapp.service.JokeServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by fossil on 23.06.18.
+ */
+@Configuration
+public class ChuckConfiguration {
+
+    @Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes() {
+        return new ChuckNorrisQuotes();
+    }
+}
