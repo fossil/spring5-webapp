e4f2a5ddf77952cb8b69bab9867cc53d28190925
Victor Pakhomov <1146089+fossil@users.noreply.github.com>
2018-07-21 16:47:52 +0200

    added several entities

---
+++ src/main/java/org/pakhomov/recipeapp/domain/UnitOfMeasure.java
@@ -0,0 +1,35 @@
+package org.pakhomov.recipeapp.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * @author Victor Pakhomov
+ * @since 14.07.18
+ */
+@Entity
+public class UnitOfMeasure {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String uom;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUom() {
+        return uom;
+    }
+
+    public void setUom(String uom) {
+        this.uom = uom;
+    }
+}
